122fdb8b599260b84e3cecdc301d9d85f74f8b17
nilesh@592 <nileshshirsat92@gmail.com>
2024-05-27 12:47:21 +0530

    first commit

---
+++ ProductMVC/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using ProductMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(ProductORM.ReturnList<ProductModel>("ViewAll"));
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int ID = 0)
+        {
+            if (ID == 0)
+                return View();
+            else
+            {
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@ID", ID);
+                return View(ProductORM.ReturnList<ProductModel>("ProductViewByID", param).FirstOrDefault<ProductModel>());
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(ProductModel Product)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("ID", Product.ID);
+            param.Add("Code", Product.Code);
+            param.Add("Name", Product.Name);
+            param.Add("Description", Product.Description);
+            param.Add("Price", Product.Price);
+            ProductORM.ExecuteWithoutReturn("ProductAddEdit", param);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@ID", id);
+            ProductORM.ExecuteWithoutReturn("ProductDeleteByID", param);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProductMVC/Models/ProductORM.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace ProductMVC.Models
+{
+    public static class ProductORM
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["Product12"].ConnectionString;
+
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+
+        //ProductORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T));
+            }
+
+        }
+
+        //ProductORM.ReturnList<ProductModel> <=  IEnumerable<ProductModel>
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+    }
+}
